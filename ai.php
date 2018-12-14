ee18973db161d2df187843d4f19b43349f2bffd7
wujunze <itwujunze@163.com>
2018-12-14 10:54:45 +0800

    add php implement

---
+++ ai.php
@@ -0,0 +1,39 @@
+<?php
+
+/**
+ * This file is part of the php-lisp/php-lisp.
+ *
+ * @Link     https://github.com/samedreams/just-for-fun-ai
+ * @Document https://github.com/samedreams/just-for-fun-ai/blob/master/README.md
+ * @Contact  itwujunze@gmail.com
+ * @License  https://github.com/samedreams/just-for-fun-ai/blob/master/LICENSE
+ *
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+
+
+/**
+ *
+ *  $10 billion AI project code implementation in PHP
+ *  
+ */
+
+function ai($input)
+{
+    $target = ['吗', '?', '？'];
+    $replace = ['', '!', '!'];
+
+    return str_replace($target, $replace, $input);
+}
+
+
+while (true) {
+    $input = readline('Me: ');
+    $output = ai($input);
+    fwrite(STDOUT, 'AI: '.$output."\n");
+}
+
+
